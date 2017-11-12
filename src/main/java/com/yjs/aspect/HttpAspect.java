996bbfba1962b19c686ea39d74052503ea113840
yujingsong1206 <646783598@qq.com>
2017-11-12 10:06:59 +0800

    增加AOP打印日志

---
+++ src/main/java/com/yjs/aspect/HttpAspect.java
@@ -0,0 +1,58 @@
+package com.yjs.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by song-pc on 2017/11/12.
+ */
+@Aspect
+@Component
+public class HttpAspect {
+
+    private final static Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
+//    @Before("execution(public * com.yjs.controller.GirlController.*(..))")
+//    public void log() {
+//        System.out.println("before");
+//    }
+
+    @Pointcut("execution(public * com.yjs.controller.GirlController.*(..))")
+    public void log() {}
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        logger.info("before");
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        //url
+        logger.info("url={}", request.getRequestURL());
+        //method
+        logger.info("method={}", request.getMethod());
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+        //类方法
+        logger.info("class_method={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        //参数
+        logger.info("args={}", joinPoint.getArgs());
+    }
+
+    //@After("execution(public * com.yjs.controller.GirlController.*(..))")
+    @After("log()")
+    public void doAfter() {
+        logger.info("after");
+    }
+
+    //显示返回结果
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object) {
+        logger.info("respons={}", object.toString());
+    }
+}
