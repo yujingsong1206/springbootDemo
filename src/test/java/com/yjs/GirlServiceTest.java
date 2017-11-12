69d077fc6aaedda13c822c0d09b9ca20d758b0dc
yujingsong1206 <646783598@qq.com>
2017-11-12 21:49:27 +0800

    增加单元测试

---
+++ src/test/java/com/yjs/GirlServiceTest.java
@@ -0,0 +1,27 @@
+package com.yjs;
+
+import com.yjs.domain.Girl;
+import com.yjs.service.GirlService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by song-pc on 2017/11/12.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirlServiceTest {
+
+    @Autowired
+    private GirlService girlService;
+
+    @Test
+    public void findOneTest() {
+        Girl girl = girlService.findOne(3);
+        Assert.assertEquals(new Integer(26), girl.getAge());
+    }
+}
