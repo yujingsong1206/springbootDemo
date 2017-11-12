fbf5382c43b78761c6dea511514beb2ca986e105
yujingsong1206 <646783598@qq.com>
2017-11-12 20:45:47 +0800

    更新自定义异常

--- src/main/java/com/yjs/handle/ExceptionHandle.java
+++ src/main/java/com/yjs/handle/ExceptionHandle.java
@@ -1,7 +1,10 @@
 package com.yjs.handle;
 
 import com.yjs.domain.Result;
+import com.yjs.exception.GirlException;
 import com.yjs.utils.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,9 +15,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class ExceptionHandle {
 
+    private final static Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
+
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Result handle(Exception e) {
-        return ResultUtil.error(e.getMessage());
+        if(e instanceof GirlException) {
+            GirlException girlException = (GirlException) e;
+            return ResultUtil.error(girlException.getCode(), girlException.getMessage());
+        } else {
+            logger.error("【系统异常】{}", e);
+            return ResultUtil.error("未知错误");
+        }
     }
 }
