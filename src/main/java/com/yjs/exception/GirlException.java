3387208cf5d76ec6a96f19fa8e0d1030048f2c79
yujingsong1206 <646783598@qq.com>
2017-11-12 20:35:03 +0800

    增加自定义异常

---
+++ src/main/java/com/yjs/exception/GirlException.java
@@ -0,0 +1,22 @@
+package com.yjs.exception;
+
+/**
+ * Created by song-pc on 2017/11/12.
+ */
+public class GirlException extends RuntimeException {
+
+    private Integer code;
+
+    public GirlException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
